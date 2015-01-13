1bfda5c11098e90d73a15f9860d3a05e4e28a403
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-01-14 00:14:44 +0100

    Added the project

---
+++ New project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    float distanceZ = 4.3f;
+    float distanceY = 7.75f;
+    float distanceX = -3.75f;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+        transform.position = new Vector3(target.position.x - distanceX, target.position.y + distanceY, target.position.z - distanceZ);
+	}
+}
---
+++ New project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour 
+{
+	public Vector3 JumpForce = new Vector3(0, 8, 0);
+	float rotationSpeed = 500f;
+
+	public GameObject blockPrefab;
+	float blockTimer = 0f;
+
+	public Transform target;
+	public float speed;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		Screen.showCursor = false;
+		//Movement
+		float speed = Input.GetAxis("Horizontal") * 10;
+		rigidbody.velocity = new Vector3(speed, rigidbody.velocity.y, rigidbody.velocity.z);
+		if (Input.GetKey(KeyCode.Space) == true)
+		{
+			rigidbody.AddForce(JumpForce);
+		}
+		float step = speed * Time.deltaTime;
+        if (target != null)
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+		//Rotation
+        float rotationX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float rotationY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotationX *= Time.deltaTime;
+        rotationY *= Time.deltaTime;
+        transform.Rotate(rotationY, rotationX, 0);
+
+		//Adding blocks
+		if (blockTimer > 0)
+			blockTimer--;
+		if (Input.GetMouseButton(0) && blockTimer == 0)
+		{
+			blockTimer = 30;
+			GameObject block = (GameObject)Instantiate(blockPrefab, new Vector3(transform.position.x, transform.position.y + 10, transform.position.z), Quaternion.identity);
+		}
+	}
+}
