0c6cff2c5e89ff28d789d1c937a69b5e943225ed
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-01-14 09:52:28 +0100

    Camera/movement stuff

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,40 +11,61 @@ public class PlayerScript : MonoBehaviour
 
 	public Transform target;
 	public float speed;
+    Vector3 pos;
 
 	void Start () 
 	{
-		
+        pos = transform.position;
 	}
 	
 	void Update () 
 	{
-		Screen.showCursor = false;
-		//Movement
-		float speed = Input.GetAxis("Horizontal") * 10;
-		rigidbody.velocity = new Vector3(speed, rigidbody.velocity.y, rigidbody.velocity.z);
-		if (Input.GetKey(KeyCode.Space) == true)
-		{
-			rigidbody.AddForce(JumpForce);
-		}
-		float step = speed * Time.deltaTime;
-        if (target != null)
-		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+		//Screen.showCursor = false;
 
-		//Rotation
+        if (Input.GetMouseButton(1))
+        {
+            pos = Input.mousePosition;
+            pos.z = 45f;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+        }
+        transform.position = Vector3.Lerp(transform.position, pos, speed * Time.deltaTime);
+
+        //Movement();
+
+        //Rotation();
+
+        //Addblocks();
+	}
+    void Movement()
+    {
+        float speed = Input.GetAxis("Horizontal") * 10;
+        rigidbody.velocity = new Vector3(speed, rigidbody.velocity.y, rigidbody.velocity.z);
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            rigidbody.AddForce(JumpForce);
+        }
+        float step = speed * Time.deltaTime;
+        if (target != null)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void Rotation()
+    {
         float rotationX = Input.GetAxis("Mouse X") * rotationSpeed;
         float rotationY = Input.GetAxis("Mouse Y") * rotationSpeed;
         rotationX *= Time.deltaTime;
         rotationY *= Time.deltaTime;
         transform.Rotate(rotationY, rotationX, 0);
+    }
 
-		//Adding blocks
-		if (blockTimer > 0)
-			blockTimer--;
-		if (Input.GetMouseButton(0) && blockTimer == 0)
-		{
-			blockTimer = 30;
-			GameObject block = (GameObject)Instantiate(blockPrefab, new Vector3(transform.position.x, transform.position.y + 10, transform.position.z), Quaternion.identity);
-		}
-	}
+    void Addblocks()
+    {
+        if (blockTimer > 0)
+            blockTimer--;
+        if (Input.GetMouseButton(0) && blockTimer == 0)
+        {
+            blockTimer = 30;
+            GameObject block = (GameObject)Instantiate(blockPrefab, new Vector3(transform.position.x, transform.position.y + 10, transform.position.z), Quaternion.identity);
+        }
+    }
 }
