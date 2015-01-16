3b81474bb7962193340d2bca0a6984e827f22e34
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-01-16 22:05:53 +0100

    More enemy stuff

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -5,6 +5,8 @@ public class EnemySpawnerScript : MonoBehaviour {
 
     public GameObject meleePrefab;
     float timer = 0f;
+    GameObject[] getCount;
+    float count;
 
 
 	void Start () 
@@ -13,9 +15,13 @@ public class EnemySpawnerScript : MonoBehaviour {
 	
 	void Update () 
     {
+        getCount = GameObject.FindGameObjectsWithTag("Enemy");
+        count = getCount.Length;
+        
+
         if (timer > 0)
             timer--;
-        if (timer <= 0)
+        if (timer <= 0 && count <= 10)
         {
             timer = 50f;
             GameObject enemy1 = (GameObject)Instantiate(meleePrefab, new Vector3(Random.Range(-20.0f, 20.0f), 1, Random.Range(-20.0f, 20.0f)), Quaternion.identity);
---
+++ Assets/Scripts/MeleeEnemyScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeEnemyScript : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 100;
+
+	void Start () 
+    {
+	}
+	
+	void Update () 
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, 1 / (speed * (Vector3.Distance(transform.position, target.position))));
+	}
+}
