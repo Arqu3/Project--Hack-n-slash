9a97cb761beb5c140416b474695d459e14fcc4d4
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-01-19 17:38:57 +0100

    Camera switching

---
+++ Assets/Scripts/SwitchCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCameraScript : MonoBehaviour {
+
+    public Camera camera1;
+    public Camera camera2;
+
+	void Start () 
+    {
+        camera1.enabled = true;
+        camera2.enabled = false;
+	}
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+	}
+}
