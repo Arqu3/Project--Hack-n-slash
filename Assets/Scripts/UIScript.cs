3b2f1b0ed6d2909495eec6070818ed127e680e23
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-01-21 23:34:16 +0100

    UI fixes + in game pause

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour {
 
     public Camera camera1;
     public Camera camera2;
+
+    bool pauseGame = false;
+
+    public Transform button;
 
 	void Start () 
     {
@@ -14,16 +19,41 @@ public class UIScript : MonoBehaviour {
 	
 	void Update () 
     {
-        if (camera2.enabled == true)
-        {
-            if (Input.GetKey(KeyCode.Escape))
-                CameraSwitch();
-        }
+        Pause();
 	}
 
     public void CameraSwitch()
     {
         camera1.enabled = !camera1.enabled;
         camera2.enabled = !camera2.enabled;
+    }
+    public void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseGame = !pauseGame;
+
+            if (pauseGame == true)
+            {
+                //Pauses the game --ENTER ALL SCRIPTS TO DISABLE HERE--
+                Time.timeScale = 0.000001f;
+                pauseGame = true;
+                GameObject.Find("Player").GetComponent<PlayerScript>().enabled = false;
+                GameObject.Find("Enemy").GetComponent<MeleeEnemyScript>().enabled = false;
+                GameObject.Find("EnemySpawner").GetComponent<EnemySpawnerScript>().enabled = false;
+                button.GetComponent<Button>().interactable = false;
+            }
+
+            if (pauseGame == false)
+            {
+                //Unpauses the game --MAKE SURE ALL SCRIPTS ARE RE-ENABLED HERE--
+                Time.timeScale = 1f;
+                pauseGame = false;
+                GameObject.Find("Player").GetComponent<PlayerScript>().enabled = true;
+                GameObject.Find("Enemy").GetComponent<MeleeEnemyScript>().enabled = true;
+                GameObject.Find("EnemySpawner").GetComponent<EnemySpawnerScript>().enabled = true;
+                button.GetComponent<Button>().interactable = false;
+            }
+        }
     }
 }
