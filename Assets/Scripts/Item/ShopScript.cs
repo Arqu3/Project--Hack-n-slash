057a9211c0c149cfce6733ec92b12cca5be6da75
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-03-11 23:47:57 +0100

    asd

--- Assets/Scripts/Item/ShopScript.cs
+++ Assets/Scripts/Item/ShopScript.cs
@@ -7,8 +7,14 @@ public class ShopScript : MonoBehaviour {
     int[] scalingCost = new int[2];
     int[] totalCost = new int[2];
 
+    GameObject player;
+    GameObject shopCanvas;
+
 	void Start () 
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        shopCanvas = GameObject.FindGameObjectWithTag("ShopCanvas");
+        shopCanvas.SetActive(false);
         baseCost = 5;
 	}
 	
@@ -17,8 +23,10 @@ public class ShopScript : MonoBehaviour {
         totalCost[0] = baseCost + scalingCost[0];
         totalCost[1] = baseCost + scalingCost[1];
 
-        if (Input.GetKeyDown(KeyCode.K))
-            AddHealth();
+        if (IsInShop(player) && Input.GetKeyDown(KeyCode.Tab))
+            shopCanvas.SetActive(true);
+        else if (!IsInShop(player))
+            shopCanvas.SetActive(false);
 	}
 
     public void AddHealth()
@@ -46,5 +54,14 @@ public class ShopScript : MonoBehaviour {
             //Apply upgrade
             PlayerScript.damage += 2;
         }
+    }
+
+    bool IsInShop(GameObject player)
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance <= 3)
+            return true;
+        else
+            return false;
     }
 }
