9ec3a9d358145f92ba13131191107c2e77dc5c79
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-03-14 00:11:29 +0100

    More block generation

--- Assets/Scripts/Map/GenerateBlockScript.cs
+++ Assets/Scripts/Map/GenerateBlockScript.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class GenerateBlockScript : MonoBehaviour {
 
-    GameObject[] blocks;
+    public GameObject[] blocks;
+    Vector3 spawnPos;
 
 	void Start () 
     {
-        for (int i = 0; i < 10000; i++)
+        spawnPos = new Vector3(0, -8, 0);
+        for (int i = 0; i < 1000; i++)
         {
-            Collider[] colliders = Physics.OverlapSphere(new Vector3(Random.Range(-201, 201), -8, Random.Range(-201, 201)), 2);
-            if(colliders.Length > 0)
-            {
-
-            }
+            //Collider[] colliders = Physics.OverlapSphere(spawnPos, 3);
+            //if (colliders.Length > 0)
+            //{
+            //    spawnPos = new Vector3(Random.Range(-201, 201), -8, Random.Range(-201, 201));
+            //}
+            //else
+            //    Instantiate(blocks[Random.Range(0, 3)], spawnPos, Quaternion.identity);
+            Instantiate(blocks[Random.Range(0, 3)], new Vector3(Random.Range(-151, 151), -8, Random.Range(-151, 151)), Quaternion.identity);
         }
 	}
 	
